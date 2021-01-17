a689d91c5a62b2fbff065739b6c094dccfb28e28
vicdudarev <V15i10c80>
2021-01-17 20:50:04 +0300

    LoginPage test modified

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,14 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, *MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     #def should_be_login_link(self):
     #    self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
--- test_main_page.py
+++ test_main_page.py
@@ -5,10 +5,20 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
+#def test_guest_can_go_to_login_page(browser):
+#    page = MainPage(browser, link)
+#    page.open()
+#    login_page = page.go_to_login_page()
+#    login_page.should_be_login_page()
+
+
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
     #try:
